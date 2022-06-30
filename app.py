6086cb0a3a9f79638d04e341ad540f4e7bf35aaa
MonCovo <s.coventry@node4.co.uk>
2022-06-30 17:29:26 +0100

    comit pls

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 from flask import Flask
+import csv
+import re
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello this is the new version for Simon!"
+@app.route("/api/customer/<telno>")
+def customerbynumber(telno):
+    telno = re.sub('[^0-9]','', telno)
+    reader = csv.DictReader(open('data/caller_info.csv', 'r'), delimiter = ',', quotechar="\"")
+    for rec in reader:
+        value = rec['Number']
+        if str(value) == str(telno):
+            return rec
+    return ('', 204)
+
+@app.route("/api/car/<telno>")
+def carbynumber(telno):
+    telno = re.sub('[^0-9]','', telno)
+    reader = csv.DictReader(open('data/car_numbers.csv', 'r'), delimiter = ',', quotechar="\"")
+    for rec in reader:
+        value = rec['number']
+        if str(value) == str(telno):
+            return rec
+    return ('', 204)
+    
